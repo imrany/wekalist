88fcdf48b4c950a3a4f14c2f05d636d20228ab64
imrany <imranmat254@gmail.com>
2025-08-06 22:17:06 +0300

    chore: v0.26.1

--- web/public/serviceworker.js
+++ web/public/serviceworker.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v0.26.0'; // also Versioning static cache
-const dynamicCache = 'site-dynamic-v0.26.0'; // Versioning dynamic cache
+const staticCacheName = 'site-static-v0.26.1'; // also Versioning static cache
+const dynamicCache = 'site-dynamic-v0.26.1'; // Versioning dynamic cache
 
 const assets = [
     // '/',
