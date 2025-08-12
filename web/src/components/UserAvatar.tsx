a5f50eac3b835e86ede43eed2edee322b346920f
imrany <imranmat254@gmail.com>
2025-08-12 17:37:00 +0300

    fix: updated UserAvatar component

--- web/src/components/UserAvatar.tsx
+++ web/src/components/UserAvatar.tsx
@@ -10,7 +10,7 @@ const UserAvatar = (props: Props) => {
   return (
     <div className={cn(`w-8 h-8 overflow-clip rounded-xl border border-border`, className)}>
       <img
-        className="w-full h-auto shadow min-w-full min-h-full object-cover"
+        className="w-8 h-8 shadow rounded-xl min-w-full min-h-full object-cover"
         src={avatarUrl || "/full-logo.webp"}
         decoding="async"
         loading="lazy"
